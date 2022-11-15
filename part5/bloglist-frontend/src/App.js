97e1d5d36d63682d3a2346a39fecbeaee5d84107
Laparv <lastappar@gmail.com>
2022-11-15 13:26:41 +0200

    5.2

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -15,6 +15,15 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if(loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      //blogService.setToken(user.token)
+    }
+  }, [])
+
   const handleLogin = async (event) => {
     event.preventDefault()
 
@@ -22,11 +31,23 @@ const App = () => {
       const user = await loginService.login({
         username, password
       })
+
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      )
+      //blogService.setToken(user.token)
+
       setUser(user)
       setUsername('')
       setPassword('')
     } catch(exception) {
     }
+    }
+
+    const handleLogout = async (event) => {
+      event.preventDefault()
+      window.localStorage.removeItem('loggedBlogappUser')
+      setUser(null)
     }
 
     const loginForm = () => (
@@ -71,7 +92,7 @@ const App = () => {
       {user === null ?
       loginForm() :
       <div>
-        <p>{user.name} logged in</p>
+        <p>{user.name} logged in <button onClick={handleLogout}>logout</button></p> 
       </div>}
       
       {blogs.map(blog =>
