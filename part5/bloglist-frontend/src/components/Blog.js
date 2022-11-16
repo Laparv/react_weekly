a1a1f387872d930288a060660d7283b270166fe7
Laparv <lastappar@gmail.com>
2022-11-16 12:07:15 +0200

    5.8

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,9 +1,11 @@
 import '../index.css'
 import { useState } from 'react'
+import blogService from '../services/blogs'
 
 const Blog = ({blog}) => {
   
   const [showAll, setShowAll] = useState(false)
+  const [newLikes, setNewLikes] = useState(blog.likes)
 
     const hideWhenVisible = {display: showAll ? 'none' : ''}
     const showWhenVisible = {display: showAll ? '' : 'none'}
@@ -12,16 +14,34 @@ const Blog = ({blog}) => {
       setShowAll(!showAll)
   }
 
+
+  const updateBlog = (event) => {
+
+    const blogObject = {
+      user: blog.user.id,
+      likes: newLikes,
+      author: blog.author,
+      title: blog.title,
+      url: blog.url
+    }
+    
+    const changedLikes = {...blogObject, likes: newLikes + 1}
+
+    blogService.update(blog.id, changedLikes).then(setNewLikes(changedLikes.likes))
+  }
+
+
   return(
-  <div class="blog" >
+  <div className="blog" >
       <div style={hideWhenVisible}>
         {blog.title} {blog.author} <button onClick={toggleVisibility}>view</button>
       </div>  
       <div style={showWhenVisible}>
         <p>{blog.title} {blog.author} <button onClick={toggleVisibility}>hide</button></p>
-        <p>{blog.likes} likes</p>
+        <p>{newLikes} likes <button onClick={updateBlog}>like</button></p> 
         <p>{blog.url}</p>
         <p>{blog.author}</p>
+        <p>{blog.user.name}</p>
       </div>  
 
   </div>
